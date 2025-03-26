e312d00359456beec494d5b01425caa02df34cf7
excniesnied <hxcn@cnies.org>
2025-03-27 00:54:31 +0800

    Remove: wwads

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,7 +7,6 @@ import './styles/vars.css'
 import './styles/landing.css'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
-import WwAds from './components/WwAds.vue'
 import ReleaseTag from './components/ReleaseTag.vue'
 import './custom.css'
 import 'virtual:group-icons.css'
@@ -16,7 +15,6 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'aside-outline-after': () => h(WwAds),
       'aside-ads-before': () => h(AsideSponsors),
     })
   },
